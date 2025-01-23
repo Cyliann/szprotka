9f38fa0c7a47b1a71a382cb78b9cbb0e4744d3b5
Cyliann <maksymilian@cych.eu>
2025-01-24 00:08:07 +0100

    Add closing on error

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ mod web;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut app = app::new()?;
-    app.get_input()?;
-    app.close(None)?;
+    if let Err(err) = app.get_input() {
+        app.close(Some(err))?;
+    }
 
-    println!("Username: {}", app.user.username);
-    app.subscribe().await?;
+    if let Err(err) = app.subscribe().await {
+        app.close(Some(err))?;
+    }
+    app.close(None)?;
 
     Ok(())
 }
